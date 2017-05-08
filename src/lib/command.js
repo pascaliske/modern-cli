baad2c862e2356757082f9e4ede559879f89c9a3
Pascal Iske <p.iske@syzygy.de>
2017-05-08 23:59:56 +0200

    feat(command): add local command execution helper for commands

--- src/lib/command.js
+++ src/lib/command.js
@@ -1,4 +1,6 @@
+import Logger from './logger';
 import Prompt from './prompt';
+import { shell } from 'execa';
 
 export default class Command {
     /* --- globals --- */
@@ -27,6 +29,25 @@ export default class Command {
     /* --- protected --- */
 
     /**
+     * Executes a given local command
+     *
+     * @param {Array} params
+     * @return {Promise}
+     */
+    async local(...params) {
+        this.log('Executing local shell command')
+        this.log.raw(Logger.grey('\n$', params.join(' ')));
+
+        const result = await shell(params, {
+            stdio: 'inherit'
+        });
+
+        this.log.raw('\n');
+
+        return result;
+    }
+
+    /**
      * Fetches necessary data from user
      *
      * @param {Array}
@@ -43,7 +64,7 @@ export default class Command {
             // start prompting for answers
             return await prompt.start();
         } catch(err) {
-            console.error(err);
+            console.error(err.message);
             process.exit(1);
         }
     }
